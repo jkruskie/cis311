e6a884a32f5142c5da91fe372ba1577812a5d9a2
Justin Kruskie <justin@jkruskie.com>
2023-03-30 16:56:48 -0400

    Upload assignments till 9

--- Assignment3/DiseaseRecord.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class DiseaseRecord
-    Public Property strPatientId As String
-    Public Property strPatientFirstName As String
-    Public Property strPatientLastName As String
-    Public Property dateVisitDate As Date
-    Public Property strDiseaseClassification As String
-End Class
